bb49b79e1d2e611384c47393e88e7289b3ab0f71
Tushar Samaniya <tusharsamaniya4956@gmail.com>
2026-01-31 08:57:32 +0530

    add the user repository

---
+++ src/main/java/com/tushar/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.tushar.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tushar.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByEmail(String email);
+
+}
