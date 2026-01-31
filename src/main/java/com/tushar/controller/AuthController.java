89075f21e149d328da4dc06f3b74117969e44427
Tushar Samaniya <tusharsamaniya4956@gmail.com>
2026-01-31 10:25:50 +0530

    created the restapi

--- src/main/java/com/tushar/controller/AuthController.java
+++ src/main/java/com/tushar/controller/AuthController.java
@@ -1,5 +1,29 @@
 package com.tushar.controller;
 
-public class AuthController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tushar.dto.UserRegrestationRequest;
+import com.tushar.service.IUserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+	
+	@Autowired
+	private IUserService userService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<String> register(@Valid @RequestBody UserRegrestationRequest request){
+		userService.register(request);
+		return ResponseEntity.ok("User registered successfully");
+	}
 }
